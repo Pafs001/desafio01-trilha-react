4db59f9b3be2bd7ed9d3153f66ebe897f7b89bd5
Pafs001 <pafs.english@gmail.com>
2021-06-07 23:38:23 -0300

    desafio concluido

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,29 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    
+    const newTask = {
+      id: Math.floor(Math.random() * 1000), title: newTaskTitle, isComplete:false
+    }
+    setTasks(task => [...task, newTask])
+    setNewTaskTitle('')    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete    
+    }
+    : task);
+    setTasks(updatedTask)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filterTask = tasks.filter(task => task.id !== id);
+    setTasks(filterTask)
   }
 
   return (
